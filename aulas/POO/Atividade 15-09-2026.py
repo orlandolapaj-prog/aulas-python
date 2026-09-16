b6829beeba4b096dfcd3a969149579f4340a7f63
Orlando <orlandolapaj@gmail.com>
2026-09-15 21:47:26 -0300

    Subindo Atividades 10/09/2026

---
+++ aulas/POO/Atividade 15-09-2026.py
@@ -0,0 +1,3 @@
+from copyreg import constructor
+
+3 Crie uma classe  que tenha no minimo 5 atributos , 1 constructor
---
+++ aulas/POO/POO.py
@@ -0,0 +1,25 @@
+# toda classe  precisa da palavara  reservada class
+Nome =[]
+for nome in Nome:
+
+
+class Aluno: # toda classe  comoça com letra  maiúscula _New
+    pass #  significado : eu vou  escrever alaguma  coisa aqui  futuramente
+
+nome_aluno= "João" # atributo -> faz referencia  a uma váriavel de
+
+obojeto
+
+def mostranomealuno(self): # metodonew*
+    # todos os metodos dentro de uma classe precisam de self
+    # uso o self
+
+print(self.nome_aluno, registro  ) # uso o self
+
+def __init__(self, nome ): # Metodo costrutor new *
+   # NOME: aLUNO .NOME_ALUNO
+
+    aluno=Aluno ("JOÃO")# ISTANCIA DA VARIAVEL NOME
+
+
+nome_de_um_aluno =
