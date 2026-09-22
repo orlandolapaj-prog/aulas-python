451a5075fcaf6d9ec6db57dcc4d311e239c31c24
Orlando <orlandolapaj@gmail.com>
2026-09-21 21:31:19 -0300

    Subindo Atividades 21/09/2026

---
+++ Atividades/21-09-2026/Classe filha.py
@@ -0,0 +1,8 @@
+# classe Filha
+#  aquivo,py     clasee
+
+from Animal import *Animal
+
+class Gato( Animal)
+    def __init__(self,idade ):
+        super.__nome = nome
---
+++ Atividades/21-09-2026/classe Pai.py
@@ -0,0 +1,8 @@
+#Classe Pai
+
+class Main:# principal -> Somente exelente codigo e objetitos
+
+    gato1 = Gato (idade 2, nome"Tom", região"Brsila")
+
+
+
